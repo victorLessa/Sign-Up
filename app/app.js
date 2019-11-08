b6cfc92fa9ecd4e7ff5c77f96e7c3c795c107b1f
Victor Lessa <victordsgnr@gmail.com>
2019-11-08 20:58:37 -0300

    middleware and route configuration file application

---
+++ app/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
+const authAutenticate = require('./config/middleware/auth-middleware')
+const errorHandler = require('./config/middleware/errorHandler-middleware')
+
+class App {
+  constructor() {
+    this.express = express()
+    this.middlewares()
+    this.routes()
+    this.express.use(errorHandler)
+  }
+  middlewares() {
+    this.express.use(cors())
+    this.express.use(authAutenticate)
+    this.express.use(express.json())
+  }
+  routes() {
+    routes(this.express)
+  }
+}
+
+module.exports = new App().express
