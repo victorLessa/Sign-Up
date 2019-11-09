778dee615929cca885a40372791389ca803f5d46
Victor Lessa <victordsgnr@gmail.com>
2019-11-09 15:19:28 -0300

    new route to fetch users and passing moment in User controler builder

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const moment = require('moment-timezone')
 let UserController = require('../controller/UserController')
-UserController = new UserController()
+UserController = new UserController(moment)
+const { sign_up_validate, sign_in_validate } = require('../validation/index')
 
-router.post('/signUp', (req, res, next) => UserController.store(req, res, next))
-router.post('/signIn', (req, res, next) =>
+router.post('/signUp', sign_up_validate, (req, res, next) =>
+  UserController.store(req, res, next)
+)
+router.post('/signIn', sign_in_validate, (req, res, next) =>
   UserController.authenticate(req, res, next)
 )
+
+router.get('/user/:id', (req, res, next) => {
+  UserController.getUser(req, res, next)
+})
 
 module.exports = router
