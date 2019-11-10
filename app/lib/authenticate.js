71c0b37c388683193b2373222648383db2d52940
Victor Lessa <victordsgnr@gmail.com>
2019-11-10 14:12:04 -0300

    refactor authenticate lib

--- app/lib/authenticate.js
+++ app/lib/authenticate.js
@@ -1,18 +1,22 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
-class Authenticate {
-  async generateToken(user_id) {
-    const token = jwt.sign({ id: user_id }, process.env.SECRET, {
-      expiresIn: process.env.EXPIRES_IN,
-    })
-    return token
-  }
-  async hashCompare(currentPassword, password) {
-    return await bcrypt.compare(currentPassword, password)
-  }
-  async hashPassword(password) {
-    return await bcrypt.hash(password, 10)
-  }
+const generateToken = async user_id => {
+  const token = await jwt.sign({ id: user_id }, process.env.SECRET, {
+    expiresIn: process.env.EXPIRES_IN,
+  })
+  return token
 }
 
-module.exports = Authenticate
+const hashCompare = async (currentPassword, password) => {
+  return await bcrypt.compare(currentPassword, password)
+}
+const hashPassword = async password => {
+  return await bcrypt.hash(password, 10)
+}
+
+module.exports = {
+  generateToken,
+  hashCompare,
+  hashPassword,
+}
+//
