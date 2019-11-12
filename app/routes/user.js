2cf1cb8f74a4c3be9e64d28d3ed37315bb00cbdd
Victor Lessa <victordsgnr@gmail.com>
2019-11-12 10:13:27 -0300

    welcome route

--- app/routes/user.js
+++ app/routes/user.js
@@ -4,6 +4,10 @@ let UserController = require('../controller/UserController')
 UserController = new UserController()
 const { sign_up_validate, sign_in_validate } = require('../validation/index')
 
+router.get('/', (req, res) => {
+  return res.send({ message: 'Bem Vindo', status: 200 })
+})
+
 router.post('/signUp', sign_up_validate, (req, res, next) =>
   UserController.store(req, res, next)
 )
